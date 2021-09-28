094b10e75193713885804b7c5f0ab596848e1453
Aldo Quintero <aldoq95@gmail.com>
2021-09-28 14:03:04 -0700

    new push

--- backend copy/database/db.js
+++ backend copy/database/db.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () =>{
   try {
-    await mongoose.connect('mongodb+srv://aldoq:Rickenbacker959$@test-db.vbr60.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+    await mongoose.connect(process.env.MONGO_URI,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
     })
